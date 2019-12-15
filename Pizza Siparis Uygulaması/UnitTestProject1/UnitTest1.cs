8bcdfc401212f3e3d5cfcd5c40e30c58b31c9575
Arda <arda.akinci12@outlook.com>
2019-12-15 19:32:41 +0300

    C# Form Uygulaması

---
+++ Pizza Siparis Uygulaması/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void  TelefonNumarası(string tel)
+        {
+            string tel = "05530911723";
+            Pizza.Form1 _hesap = new Pizza.Form1();
+            string sonuc = 
+        }
+    }
+}
